0d10ba8986ead239d270a3292acefa3a0a7af5a4
MNandaNH <89273906+MNandaNH@users.noreply.github.com>
2021-08-26 00:12:34 -0500

    Add files via upload

---
+++ modulos/ModuloFib/Test2/Fibuse.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 23 10:01:45 2021
+
+@author: franc
+"""
+
+import Fibo
+
+Fibo.fibonacci(400)
---
+++ modulos/Test1/modulo.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 23 09:21:12 2021
+
+@author: franc
+"""
+print("Genial! Soy un módulo")
